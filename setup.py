b43d826b6858d58fec8f7ecd470c0f287a9d6799
Titus M <86983072+titushm@users.noreply.github.com>
2023-03-29 18:15:21 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'pathnav',
+  packages = ['pathnav'],
+  version = '1.0',
+  license='MPL-2',
+  description = 'Navigate paths with ease',
+  author = 'TitusHM',
+  author_email = '',
+  url = 'https://github.com/titushm/pathnav',
+  download_url = 'https://github.com/titushm/pathnav/archive/refs/heads/main.zip',
+  keywords = ['path', 'navigation', 'path edit'],
+  install_requires=[],
+  classifiers=[
+	'Development Status :: 4 - Beta',
+	'Intended Audience :: Developers',
+	'Topic :: Software Development :: Build Tools',
+	'License :: OSI Approved :: MPL-2',
+	'Programming Language :: Python :: 3.5',
+	'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: 3.7',
+	'Programming Language :: Python :: 3.8',
+	'Programming Language :: Python :: 3.9',
+	'Programming Language :: Python :: 3.10',
+	'Programming Language :: Python :: 3.11'
+  ],
+)
