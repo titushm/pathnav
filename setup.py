39605d1e05d43aaf28602ea8a5e95eb49070888f
Titus <titusmeader@btinternet.com>
2023-12-24 20:04:38 +0000

    initial

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as f:
     long_description = f.read()
 
+
 VERSION = '0.0.1' 
 DESCRIPTION = 'A command-line GUI for Python'
 
