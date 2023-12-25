126e7e3e95e43497583d5fd9c30d0af6367666a3
Titus M <86983072+titushm@users.noreply.github.com>
2023-12-25 09:26:06 +0000

    Revert "initial"
    This reverts commit 39605d1e05d43aaf28602ea8a5e95eb49070888f.

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-
 VERSION = '0.0.1' 
 DESCRIPTION = 'A command-line GUI for Python'
 
